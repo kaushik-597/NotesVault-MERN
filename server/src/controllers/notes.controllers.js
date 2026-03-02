c120b458b6b43b27372dbff418fe138ace8a40ea
Saurav Kaushik <sauravkaushik597@gmail.com>
2026-03-02 17:16:35 +0530

    add delete note controller and route

--- server/src/controllers/notes.controllers.js
+++ server/src/controllers/notes.controllers.js
@@ -1,3 +1,4 @@
+import { Notes } from "../models/notes.models.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
@@ -9,17 +10,18 @@ const addNote = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Title and Description are required :)");
   }
 
+  const note = await Notes.create({ title, description });
   try {
-    return res.json(
-      new ApiResponse(
-        200,
-        { title, description },
-        "Note added successfully :D",
-      ),
-    );
+    return res.json(new ApiResponse(200, note, "Note added successfully :D"));
   } catch (error) {
     throw new ApiError(401, "Bad request");
   }
 });
 
-export { addNote };
+const delNote = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const note = await Notes.findByIdAndDelete(id);
+  return res.json(new ApiResponse(200, note, "Note deleted successfully XD"));
+});
+
+export { addNote, delNote };
--- server/src/routes/notes.routes.js
+++ server/src/routes/notes.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { addNote } from "../controllers/notes.controllers.js";
+import { addNote, delNote } from "../controllers/notes.controllers.js";
 
 const router = Router();
 
 router.route("/add").post(addNote);
+router.route("/del").delete(delNote);
 
 export default router;
